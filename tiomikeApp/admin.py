7a23b427100edb2877b8f4d6e18e84c27641aa16
mintic <josejimene3>
2022-05-15 14:04:58 -0500

    creacion de modelos 1, 5, 7 y 14

--- tiomikeApp/admin.py
+++ tiomikeApp/admin.py
@@ -1,3 +1,13 @@
+from importlib import import_module
 from django.contrib import admin
 
 # Register your models here.
+from .models.barrio import Barrio
+from .models.departamento import Departamento
+from .models.detalleFactura import DetalleFactura
+from .models.proveedor import Proveedor
+
+admin.site.register(Barrio)
+admin.site.register(Departamento)
+admin.site.register(DetalleFactura)
+admin.site.register(Proveedor)
