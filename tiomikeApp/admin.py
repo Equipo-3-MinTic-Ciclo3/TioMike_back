6c3edf3dda58940f676f6cb06c912acca7819295
Daniel A. Melo Salamanca <danielmelo19971@gmail.com>
2022-05-15 20:19:10 -0500

    modelos registrados en el archivo admin

--- tiomikeApp/admin.py
+++ tiomikeApp/admin.py
@@ -6,6 +6,10 @@ from .models.barrio import Barrio
 from .models.departamento import Departamento
 from .models.detalleFactura import DetalleFactura
 from .models.proveedor import Proveedor
+from .models.detalleMascota import DetalleMascota
+from .models.ciudad import Ciudad
+from .models.genero import Genero
+from .models.factura import Factura
 
 admin.site.register(Barrio)
 admin.site.register(Departamento)
