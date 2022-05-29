98f2135d45acdcc9144fad4a61fb13f530e9caf4
Sandra Milena Giraldo Isaza <sandragiraldo3022@gmail.com>
2022-05-28 19:30:27 -0500

    Serializers Realizados

--- tiomikeApp/serializers/clienteSerializer.py
+++ tiomikeApp/serializers/clienteSerializer.py
@@ -13,7 +13,35 @@ class ClienteSerializer(serializers.ModelSerializer):
     tipoIdentificacion = TipoIdentificacionSerializer()
     class Meta:
         model = Cliente
-        fields = ['id', 'username', 'password', 'nombre', 'email', 'rol', 'estado']
+        fields = ['idCliente', 'cliente_tipo_identificacion', 'nombres', 'apellidos', 'fechaNacimiento', 'idGenero', 'idCiudad', 'edad', 'telefono', 'email', 'direccion', 'idBarrio', 'estado']
+        
+    def create(self, validated_data):
+        tipoIdentificacionData = validated_data.pop('cliente_tipo_identificacion')
+        clienteInstance = Cliente.objects.create(**validated_data)
+        TipoIdentificacion.objects.create(usuario = clienteInstance, **tipoIdentificacionData)
+        return clienteInstance
+    
+    def to_representation(self, obj):
+        cliente = Cliente.objects.get(idCliente=obj.cliente_tipo_identificacion)
+        tipoIdentificacion = TipoIdentificacion.objects.get(cliente=obj.cliente_tipo_identificacion)
+        return {
+                    'idCliente':cliente.idCliente, 
+                    'nombres':cliente.nombres, 
+                    'apellidos':cliente.apellidos, 
+                    'fechaNacimiento':cliente.fechaNacimiento,
+                    'edad':cliente.edad, 
+                    'telefono':cliente.telefono, 
+                    'email':cliente.email, 
+                    'direccion':cliente.direccion, 
+                    'estado':cliente.estado,
+                    'tipoIdentificacion': {
+                        'idTipoIdentificacion':tipoIdentificacion.idTipoIdentificacion,
+                        'nomTipoIdentificacion':tipoIdentificacion.nomTipoIdentificacion,
+                        'estado':tipoIdentificacion.estado
+                        
+                    }                    
+        }
+        
 
 
 
